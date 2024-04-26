53d5655d9958cd4cb834d2f4dfed6e2ef50f841e
Dustin Carlino <dabreegster@gmail.com>
2024-04-26 15:07:37 +0100

    Seed with the simple things

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # utils
-Common Rust code used in several projects
+
+Common Rust code used in several projects.
+
+This is a collection of random things used in a few places. All of the code here should ultimately be upstreamed in anothr crate, or split into a proper crate. It's not easy to do that, and in the meantime, copies of this code are spreading, so at least as a first step, deduplicating here.
---
+++ src/node_map.rs
@@ -0,0 +1,40 @@
+// TODO This is a version with no serialization. Upstream?
+
+use std::collections::BTreeMap;
+use std::fmt::Debug;
+
+use fast_paths::NodeId;
+
+/// A bidirectional mapping between fast_paths NodeId and some custom ID type.
+#[derive(Clone)]
+pub struct NodeMap<T: Copy + Ord + Debug> {
+    node_to_id: BTreeMap<T, NodeId>,
+    id_to_node: Vec<T>,
+}
+
+impl<T: Copy + Ord + Debug> NodeMap<T> {
+    pub fn new() -> NodeMap<T> {
+        NodeMap {
+            node_to_id: BTreeMap::new(),
+            id_to_node: Vec::new(),
+        }
+    }
+
+    pub fn get_or_insert(&mut self, node: T) -> NodeId {
+        if let Some(id) = self.node_to_id.get(&node) {
+            return *id;
+        }
+        let id = self.id_to_node.len();
+        self.node_to_id.insert(node, id);
+        self.id_to_node.push(node);
+        id
+    }
+
+    pub fn get(&self, node: T) -> Option<NodeId> {
+        self.node_to_id.get(&node).cloned()
+    }
+
+    pub fn translate_id(&self, id: usize) -> T {
+        self.id_to_node[id]
+    }
+}
